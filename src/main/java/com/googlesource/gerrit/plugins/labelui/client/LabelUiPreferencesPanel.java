6789ee22391bd9842a361a7e769f8e755ac7f323
Edwin Kempin <edwin.kempin@sap.com>
2015-07-14 13:57:52 +0200

    Plugin that implements a table to render labels in change screen
    This plugin adds a user preference that allows users to choose a table
    control to render the labels/approvals on the change screen (similar
    to how labels/approvals were rendered on the old change screen).
    
    Some users prefer the table view because it's easier to see which user
    has already voted and whether there are users that haven't voted yet.
    
    Change-Id: I57e8d5137f95f9a2553b2090ee0cb911fafc4c6b
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/labelui/client/LabelUiPreferencesPanel.java
@@ -0,0 +1,107 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.labelui.client;
+
+import com.google.gerrit.plugin.client.Plugin;
+import com.google.gerrit.plugin.client.extension.Panel;
+import com.google.gerrit.plugin.client.rpc.RestApi;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import com.googlesource.gerrit.plugins.labelui.client.PreferencesInfo.LabelUi;
+
+public class LabelUiPreferencesPanel extends VerticalPanel {
+  private final static String HIDE_LABELS_CLASS = Plugin.get().getName() + "-hideLabels";
+
+  static class Factory implements Panel.EntryPoint {
+    @Override
+    public void onLoad(Panel panel) {
+      panel.setWidget(new LabelUiPreferencesPanel());
+    }
+  }
+
+  LabelUiPreferencesPanel() {
+    new RestApi("accounts").id("self")
+        .view(Plugin.get().getPluginName(), "preferences")
+        .get(new AsyncCallback<PreferencesInfo>() {
+          @Override
+          public void onSuccess(PreferencesInfo result) {
+            display(result);
+          }
+
+          @Override
+          public void onFailure(Throwable caught) {
+            // never invoked
+          }
+        });
+  }
+
+  private void display(PreferencesInfo info) {
+    Label heading = new Label(Plugin.get().getName() + " plugin");
+    heading.setStyleName("smallHeading");
+    add(heading);
+    HorizontalPanel p = new HorizontalPanel();
+    add(p);
+
+    Label label = new Label("Label UI:");
+    p.add(label);
+    label.getElement().getStyle().setMarginRight(5, Unit.PX);
+    label.getElement().getStyle().setMarginTop(2, Unit.PX);
+    final ListBox ui = new ListBox();
+    p.add(ui);
+
+    for (PreferencesInfo.LabelUi v : PreferencesInfo.LabelUi.values()) {
+      ui.addItem(v.name(), v.name());
+    }
+
+    for (int i = 0; i < ui.getItemCount(); i++) {
+      if (info.ui().name().equals(ui.getValue(i))) {
+        ui.setSelectedIndex(i);
+        break;
+      }
+    }
+
+    ui.addChangeHandler(new ChangeHandler() {
+      @Override
+      public void onChange(ChangeEvent event) {
+        PreferencesInfo info = PreferencesInfo.create();
+        info.ui(ui.getSelectedValue());
+        new RestApi("accounts").id("self")
+            .view(Plugin.get().getPluginName(), "preferences")
+            .put(info, new AsyncCallback<PreferencesInfo>() {
+              @Override
+              public void onSuccess(PreferencesInfo result) {
+                RootPanel.getBodyElement().removeClassName(HIDE_LABELS_CLASS);
+                if (result.ui() != LabelUi.DEFAULT) {
+                  RootPanel.getBodyElement().addClassName(HIDE_LABELS_CLASS);
+                }
+              }
+
+              @Override
+              public void onFailure(Throwable caught) {
+                // never invoked
+              }
+            });
+      }
+    });
+  }
+}
